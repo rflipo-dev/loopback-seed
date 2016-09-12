dce86ee4cbedaad2e09e55e73b6aa40e3d77d350
Rémi Flipo <rflipo@wemanity.com>
2016-09-12 13:52:26 +0200

    fix options

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import LoopbackSeed from './lib/loopbackSeed';
 
 module.exports = function(app, options) {
-  let seeder = new LoopbackSeed();
+  let seeder = new LoopbackSeed(app, options);
   app.seeder = seeder;
 };
